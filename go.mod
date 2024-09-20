790c5a0dd7900a22289098303e2493fcbd1f6a34
Alexander Terp <alexander.terp@gmail.com>
2024-09-20 20:43:48 +1000

    Fork as go-tbl
    Just the bureaucracy associated.

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/olekukonko/tablewriter
+module github.com/AlexanderTerp/go-tbl
 
 go 1.12
 
--- table.go
+++ table.go
@@ -1,11 +1,3 @@
-// Copyright 2014 Oleku Konko All rights reserved.
-// Use of this source code is governed by a MIT
-// license that can be found in the LICENSE file.
-
-// This module is a Table Writer  API for the Go Programming Language.
-// The protocols were written in pure Go and works on windows and unix systems
-
-// Package tablewriter Create & Generate text based table
 package tablewriter
 
 import (
